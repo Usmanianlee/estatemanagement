226e29e847bcbff619dc36d994738d6ad7be9bbd
usmanianlee <olokousman0@gmail.com>
2024-10-11 06:38:38 -0700

    FEAT: fixing

--- src/app/components/All.jsx
+++ src/app/components/All.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import Link from 'next/link'
-import MovieCard from './MovieCard'
-import { movies } from '../data'
+import React from "react";
+import Link from "next/link";
+import MovieCard from "./MovieCard";
+import { movies } from "../data";
 
 export default function All() {
-    const forAll = movies.filter(card => card.Type == 'Sale')
+  const forAll = movies.filter((card) => card.Type == "Sale");
 
-    const forAllCard = forAll.map((card, index) =>
-        <Link href={`/movies/${card.Title.split(' ').join('-')}`} title={card.Title} className='cursor-pointer' key={index}>
-        <MovieCard title={card.Title} type={card.Type} rat ={card.rating} img={card.image} amt={card.Amount} />
-        </Link>
-    )
-    
-  return (
-    <div>
-      {forAllCard}
-    </div>
-  )
+  const forAllCard = forAll.map((card, index) => (
+    <Link
+      href={`/movies/${card.Title.split(" ").join("-")}`}
+      title={card.Title}
+      className="cursor-pointer"
+      key={index}
+    >
+      <MovieCard
+        title={card.Title}
+        type={card.Type}
+        rat={card.rating}
+        img={card.image}
+        amt={card.Amount}
+      />
+    </Link>
+  ));
+
+  return <div>{forAllCard}</div>;
 }
